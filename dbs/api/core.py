d53593ad6a31a7cbc20788dc1a23739ec02e818b
Tomas Tomecek <ttomecek@redhat.com>
2014-11-11 16:03:28 +0100

    build results: dont tb when response miss some keys, defensive coding

--- dbs/api/core.py
+++ dbs/api/core.py
@@ -9,6 +9,7 @@ from django.core.exceptions import ObjectDoesNotExist
 
 from dbs.models import Task, TaskData, Dockerfile, Image
 from dbs.task_api import TaskApi
+from dbs.utils import chain_dict_get
 
 
 logger = logging.getLogger(__name__)
@@ -27,17 +28,27 @@ def new_image_callback(task_id, response_tuple):
     t = Task.objects.filter(id=task_id).first()
     t.date_finished = datetime.now()
     if response_hash:
-        image_id = response_hash['built_img_info']['Id']
-        parent_image_id = response_hash['base_img_info']['Id']
-        image_tags = response_hash['built_img_info']['RepoTags']
-        parent_tags = response_hash['base_img_info']['RepoTags']
-        parent_image = Image.create(parent_image_id, Image.STATUS_BASE, tags=parent_tags)
-        df_model = Dockerfile(content=df)
-        df_model.save()
-        image = Image.create(image_id, Image.STATUS_BUILD, tags=image_tags,
-                             task=t, parent=parent_image, dockerfile=df_model)
-        image.add_rpms_list(response_hash['built_img_plugins_output']['all_packages'])
-        parent_image.add_rpms_list(response_hash['base_plugins_output']['all_packages'])
+        image_id = chain_dict_get(response_hash, ['built_img_info', 'Id'])
+        parent_image_id = chain_dict_get(response_hash, ['base_img_info', 'Id'])
+        image_tags = chain_dict_get(response_hash, ['built_img_info', 'RepoTags'])
+        parent_tags = chain_dict_get(response_hash, ['base_img_info', 'RepoTags'])
+        if image_id and parent_image_id and image_tags and parent_tags:
+            parent_image = Image.create(parent_image_id, Image.STATUS_BASE, tags=parent_tags)
+            image = Image.create(image_id, Image.STATUS_BUILD, tags=image_tags,
+                                 task=t, parent=parent_image)
+            if df:
+                df_model = Dockerfile(content=df)
+                df_model.save()
+                image.dockerfile = df_model
+                image.save()
+            rpm_list = chain_dict_get(response_hash, ['built_img_plugins_output', 'all_packages'])
+            base_rpm_list = chain_dict_get(response_hash, ['base_plugins_output', 'all_packages'])
+            if rpm_list:
+                image.add_rpms_list(rpm_list)
+            if base_rpm_list:
+                image.add_rpms_list(base_rpm_list)
+        else:
+            t.status = Task.STATUS_FAILED
 
         t.status = Task.STATUS_SUCCESS
     else:
---
+++ dbs/utils.py
@@ -0,0 +1,19 @@
+__author__ = 'ttomecek'
+
+
+def chain_dict_get(dict_object, keys_list, default=None):
+    """
+    something like {}.get('key1', 'key2', 'key3', default='') which means
+
+    {}['key1']['key2]...
+
+    :param dict_object: dict
+    :param keys_list: list of keys
+    :return: value or default
+    """
+    d = dict_object
+    for key in keys_list:
+        try:
+            d = d[key]
+        except (KeyError, TypeError):
+            return default
