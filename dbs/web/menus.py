a8f59bf5f1b50771fb3f74bc12330ba68aef1235
Tim Waugh <twaugh@redhat.com>
2014-11-18 17:30:23 +0000

    Add back in menu items for parts without templates yet.

--- dbs/web/menus.py
+++ dbs/web/menus.py
@@ -14,3 +14,10 @@ Menu.add_item("main", MenuItem("Images",
                                "/images",
                                weight=30))
 
+Menu.add_item("main", MenuItem("Build a New Image",
+                               "/new",
+                               weight=40))
+
+Menu.add_item("main", MenuItem("Move Image",
+                               "/move",
+                               weight=50))
